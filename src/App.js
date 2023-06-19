d4be757ddcf20ba5f87370b217dda42539fb6e8d
maxfan-precalculus <xfan.ncsu@gmail.com>
2023-06-19 13:56:01 -0400

    init commit of hacker-stories

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {useState} from 'react';
 
-function App() {
+const App = () => {
+  const stories =[{"title":"React","author":"Adeline"},
+      {"title":"Research","author":"Sophia"}]
+  const [search, setSearch] = useState();
+  const filteredStories = stories.filter((item) => item.title.toLowerCase == search);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Search></Search>
+      <List stories={filteredStories}></List>
     </div>
   );
 }
+
+const Search = (props) => {
+  <div>
+      <label htmlFor='search'>Search:</label>
+      <input id='search'></input>
+  </div>
+
+}
+const List = (props) => (
+   props.stories.map( (item) => (
+      <li><Item item={item}></Item></li>
+    )
+   )
+)
+const Item = (props) => (
+  <div>
+ <label>{props.item.title}</label> 
+ <label>{props.item.author}</label> 
+ </div>
+)
 
 export default App;
