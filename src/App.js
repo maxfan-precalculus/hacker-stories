bac6bbd0e0fe52dbf2bba9ea3539ff23f15b4776
xfan.ncsu@gmail.com <xfan.ncsu@gmail.com>
2023-06-24 20:14:48 -0400

    useEffect implemented

--- src/App.js
+++ src/App.js
@@ -1,19 +1,42 @@
 import React from 'react'
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 
 const App = () => {
-  const stories =[{"title":"React","author":"Adeline"},
+  const initialStories =[{"title":"React","author":"Adeline"},
       {"title":"Research","author":"Sophia"},{"title":"Redux", "author":"Max"} ]
-  const [searchTerm, setSearch] = useState("");
+  const [stories, setStories] = useState([]);
+
+  // return a simulation promise
+  const getAsyncStories = () => {
+    return new Promise( (resolve) => {
+      setTimeout( () => resolve({data:{stories:initialStories}}),3000)
+    })
+  }
+
+  useEffect( () => { 
+    getAsyncStories().then(result => {
+      setStories(result.data.stories);
+    }
+      );
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+  const [searchTerm, setSearchTerm] = useState("");
   const handleSearch = (event) => {
-    setSearch(event.target.value);
+    setSearchTerm(event.target.value);
+  }
+
+  const handleRemove = (item) => {
+    setStories(
+      stories.filter( (story) => story.title !== item.title)
+    );
   }
 
   const filteredStories = stories.filter((item) => item.title.toLowerCase().includes(searchTerm.toLowerCase()));
   return (
     <div>
     <Search handleSearch={handleSearch}></Search>
-    <List stories={filteredStories}></List>
+    <List stories={filteredStories} handleRemove={handleRemove}></List>
     </div>
   );
 }
@@ -28,7 +51,7 @@ const Search = (props) => (
 const List = (props) => (
   <ul>
    {props.stories.map( (item) => (
-      <li><Item item={item}></Item></li>
+      <li><Item item={item} handleRemove={props.handleRemove}></Item></li>
     ))
    }
   </ul>
@@ -37,6 +60,7 @@ const Item = (props) => (
   <div>
  <label>{props.item.title}</label> 
  <label>{props.item.author}</label> 
+ <button onClick={ () => props.handleRemove(props.item)}>Click to remove</button>
  </div>
 )
 
